b4340942dcb4ad048d773a92f4ee857218e09732
pushpendra <pushpendra.singh@iitkgp.ac.in>
2018-01-23 20:09:52 +0530

    Update ohm_meter.ino

--- ohm_meter/ohm_meter.ino
+++ ohm_meter/ohm_meter.ino
@@ -1,19 +1,28 @@
-int R1=976;
-double x=0;
-double R2;
-void setup() {
-  Serial.begin(9600);
-  pinMode(A0, INPUT);
-  // put your setup code here, to run once:
-
+int analogPin = 0; // reads the resistance of R2
+int raw = 0; // variable to store the raw input value
+int Vin = 5; // variable to store the input voltage
+float Vout = 0; // variable to store the output voltage
+float R1 = 1000; // variable to store the R1 value
+float R2 = 0; // variable to store the R2 value
+float buffer = 0; // buffer variable for calculation
+void setup()
+{
+Serial.begin(9600); // Set up serial
 }
-
-void loop() {
-  x=analogRead(A0);
-  R2=(x/(1023-x))*R1;
+void loop()
+{
+raw = analogRead(analogPin); // reads the input pin
+if(raw)
+{
+  buffer=raw*Vin;
+  Vout=buffer/1024;
+  buffer=(Vin/Vout)-1;
+  R2=R1/buffer;
+  Serial.print("Vout:");
+  Serial.println(Vout);
+  Serial.print("R2:");
   Serial.println(R2);
   delay(1000);
-  // put your main code here, to run repeatedly:
-  //ok
+}
 
 }
